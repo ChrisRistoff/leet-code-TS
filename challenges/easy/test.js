5aaeb2104cbaabdb8c31a8824fa0028d0f50314d
Chris Ristoff <doanusho@gmail.com>
2024-09-30 20:38:00 +0100

    Initial setup, CI should work.

---
+++ challenges/easy/test.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.doMath = doMath;
+function doMath(num) {
+    return num + 10;
+}
+console.log(doMath(5));
---
+++ dist/__tests__/easy/doMath.test.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const test_1 = require("../../challenges/easy/test");
+describe('Test test', () => {
+    it('should return 15', () => {
+        expect((0, test_1.doMath)(10)).toEqual(15);
+    });
+});
